99151a85ee8af4ce7a9f0051878150af3222aee9
Ефим <fimakrut@mail.ru>
2024-06-13 16:09:57 +0500

    Завершение создания всех классов. Ярыгин

---
+++ uchot_oborud.Yar/Classes/LoginContext.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uchot_oborud.Yar.Classes.Common;
+using uchot_oborud.Yar.Models;
+
+namespace uchot_oborud.Yar.Classes
+{
+    public class LoginContext : Login
+    {
+        public LoginContext(int Id, string Name, string Password) : base(Id, Name, Password) { }
+        public static List<LoginContext> Select()
+        {
+            List<LoginContext> allLogins = new List<LoginContext>();
+            string SQL = "SELECT * FROM `Login`;";
+            MySqlConnection connection = Connection.OpenConnection();
+            MySqlDataReader Data = Connection.Query(SQL, connection);
+            while (Data.Read())
+            {
+                allLogins.Add(new LoginContext(
+                    Data.GetInt32(0),
+                    Data.GetString(1),
+                    Data.GetString(2)));
+            }
+            Connection.CloseConnection(connection);
+            return allLogins;
+        }
+    }
+}
