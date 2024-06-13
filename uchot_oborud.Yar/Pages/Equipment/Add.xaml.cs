98bbc0f2edb41e6a1776e458aa3e4d5ce0487989
Ефим <fimakrut@mail.ru>
2024-06-13 17:17:57 +0500

    Почти готовая учебная практика

--- uchot_oborud.Yar/Pages/Equipment/Add.xaml.cs
+++ uchot_oborud.Yar/Pages/Equipment/Add.xaml.cs
@@ -1,23 +1,26 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Interop;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows.Media.Animation;
+using Imaging = Aspose.Imaging;
 using uchot_oborud.Yar.Classes;
+using System.Diagnostics;
+
+
 
 namespace uchot_oborud.Yar.Pages.Equipment
 {
---
+++ uchot_oborud.Yar/Pages/Status/Main.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using uchot_oborud.Yar.Classes;
+
+namespace uchot_oborud.Yar.Pages.Status
+{
+    public partial class Main : Page
+    {
+        List<StatusContext> AllStatuses = StatusContext.Select();
+
+        public Main()
+        {
+            InitializeComponent();
+
+            foreach (StatusContext item in AllStatuses)
+            {
+                parent.Children.Add(new Items.Item(item, this));
+            }
+
+            if (MainWindow.init.ActiveUser.Role == "Пользователь")
+            {
+                Add.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void AddRecord(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.OpenPages(new Pages.Status.Add());
+        }
+
+        private void SearchStatus(object sender, KeyEventArgs e)
+        {
+            string search = tbSearch.Text;
+            parent.Children.Clear();
+            foreach (StatusContext classroom in AllStatuses)
+            {
+                if (classroom.Name.ToLower().Contains(search.ToLower()))
+                {
+                    parent.Children.Add(new Items.Item(classroom, this));
+                }
+            }
+        }
+    }
+}
